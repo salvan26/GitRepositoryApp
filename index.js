192ec3d05c974861a70ee24c4bb66d1362bf1a9e
Sergei Demidov <Salvan26@mail.ru>
2024-10-03 14:42:52 +0300

    sang

--- index.js
+++ index.js
@@ -1,115 +1,134 @@
-const search = document.querySelector(".repository__search");
-const autocomplete = document.querySelector('.repository__autocomplete');
-const repositoryList = document.querySelector('.repository__list');
-const gitAPI = 'https://api.github.com/search/repositories?q=';
-const app = document.querySelector('.repository_form')
+class ApplicationView {
+    constructor(){
+        this.app = document.getElementById('app');
+        this.searchField = this.createElement('input', 'search-field');
+        this.searchField.setAttribute('type', 'text');
+        this.searchField.setAttribute('placeholder', 'Введите текст...');
+        this.wrapper = this.createElement('div', 'wrapper');
+        this.repositoryList = this.createElement('ul', 'repositoryList');
+        this.favotiteRep = this.createElement('div', 'favotite-rep');
+        this.buttonClose = this.createElement('button', 'button-close')
+        this.wrapper.append(this.searchField);
+        this.wrapper.append(this.repositoryList);
+        
+        
+        this.app.append(this.wrapper);
+        this.app.append(this.favotiteRep);
 
+        
+    }
 
-function clearElem (parent){
-    parent.innerHTML = "";
-}
-function clearValue (element){
-    element.value = "";
-}
+    createElement(elementTag, elementClass) {
+        const element = document.createElement(elementTag);
+        if(elementClass) {
+            element.classList.add(elementClass);
+        }
+        return element;
+    }
 
-function createChild (parent, child){
-    parent.append(child)
-}
+    clearValue (element){
+        element.value = "";
+    }
 
-function  delBtnClickHandler (){
-    const elem = this.parentNode;
-    repositoryList.removeChild(elem);
-}
+   createRepository( { name, owner, stargazers_count }){
+        return `<li class="repository__li">
+            <p class = "repository__item"> Name: ${name}</p>
+            <p class = "repository__item"> Owner: ${owner.login} </p>
+            <p class = "repository__item"> Stars: ${stargazers_count}</p>
+            <button class = "repository__button button-close"></button>
+             </li> `;
+    }
 
-function  createRepository( { name, owner, stargazers_count }){
-    return `<li class="repository__li">
-        <p class = "repository__item"> Name: ${name}</p>
-        <p class = "repository__item"> Owner: ${owner.login} </p>
-        <p class = "repository__item"> Stars: ${stargazers_count}</p>
-        <button class = "repository__button button-close"></button>
-         </li> `;
-}
+    repoClickHandler(repo){
+        const repoli = createRepository(repo);
+        const newRepositoryItem = createNewElement(repoli);
+        createChild(repositoryList, newRepositoryItem);
+        const delRepoBtn = newRepositoryItem.querySelector('.button-close');
+        delRepoBtn.addEventListener('click', delBtnClickHandler);
+        clearValue(this.searchField);
+    }
 
-function createNewElement(item){
-  const newElement = document.createElement('li');
-  newElement.innerHTML = item;
-  return newElement.firstChild;
-}
+    createChild (parent, child){
+        parent.append(child)
+    }
 
-function repoClickHandler(repo){
-    const repoli = createRepository(repo);
-    const newRepositoryItem = createNewElement(repoli);
-    createChild(repositoryList, newRepositoryItem);
-    const delRepoBtn = newRepositoryItem.querySelector('.button-close');
-    delRepoBtn.addEventListener('click', delBtnClickHandler);
-    clearValue(search);
-}
+    delBtnClickHandler (){
+        const elem = this.parentNode;
+        repositoryList.removeChild(elem);
+    }
 
+    createNewElement(item){
+        const newElement = document.createElement('li');
+        newElement.innerHTML = item;
+        return newElement.firstChild;
+      }
 
-function autocompleteSearch(RepoDate){
-    if (autocomplete.firstChild)  clearElem(autocomplete);
-    return RepoDate.forEach( elem => {
-        const newItem = `<li class = "autocomplete__li">${elem.name}</li>`;
-        const newElement =  createNewElement(newItem);
-        createChild(autocomplete, newElement);
+      autocomplete(repData){
+        const newItem = this.createElement('li');
+        newItem.innerHTML = `<p>${repData.name}</p>`;
+        const newElement = this.createNewElement(newItem);
+        this.repositoryList.append(newItem);
+        this.createChild(this.repositoryList, newElement);
         newElement.addEventListener('click', ()=> repoClickHandler(elem));
-    })
-}
+  
+    }
 
-async function  getRepoData(repoName){
-  return  await fetch(`${gitAPI}${repoName}`)
-      .then( response => response.json())
-      .then( res => res.items.slice(0,5))
-      .then( res => autocompleteSearch(res))
-      .catch( e => console.log('Error', e));
+    
+
+    
 }
 
 
-const debounce = (cb, debounceTime) => {
-    let debounceActive;
-    return function () {
-        clearTimeout(debounceActive);
-        debounceActive = setTimeout(() =>  
-          cb.apply( this, arguments), debounceTime);
+class Search {
+    constructor(ApplicationView) {
+        this.ApplicationView = ApplicationView;
+        this.ApplicationView.searchField.addEventListener('keyup', this.debounce(this.searchUsers.bind(this), 300))
+    }
+
+    debounce = (fn, debounceTime) => {
+        let timer;
+    
+        return function (...args) {
+            clearTimeout(timer);
+            timer = setTimeout(() => {
+                fn.apply(this, args)
+            }, debounceTime)
+        }
+     };
+   
+     updateUsers(response, isUpdate = false) {
+        let users;
+        if (response.ok) {
+            if (!isUpdate) {
+                this.clearField();
+            }
+            response.json().then((res) => {
+                if (res.items) {
+                    users = res.items;
+                    users.forEach(user => this.ApplicationView.autocomplete(user));
+                } else {
+                    this.clearField();
+                }
+            });
+        } else {
+            console.log('Error 1' + response.status);
+        }
+    } 
+
+    searchUsers() {
+        if (this.ApplicationView.searchField.value) {
+            this.loadUsers(this.ApplicationView.searchField.value).then(response => this.updateUsers(response))
+        } else {
+            this.clearField();
+        }
+    }
+    
+    async loadUsers(searchValue) {
+        return await fetch(`https://api.github.com/search/repositories?q=${searchValue}&per_page=5`);
+    }
+    clearField () {
+        this.ApplicationView.repositoryList.innerHTML = '';
     }
 }
 
-const debounceRepoDate = debounce(getRepoData, 300);
-
-function windowEscHandler(evt){
-    if(evt.keyCode == 27) {
-        clearElem(autocomplete);
-        window.removeEventListener('keydown', windowEscHandler);
-        window.removeEventListener('click', windowClickHandler);
-        search.addEventListener('click', searchClickHandler);
-    }
-}
-
-function windowClickHandler(evt){
-    if( evt.target!== search ) {
-        clearElem(autocomplete);
-        window.removeEventListener('keydown', windowEscHandler);
-        window.removeEventListener('click', windowClickHandler);
-        search.addEventListener('click', searchClickHandler);
-    }
-}
-
-function searchClickHandler(evt) {
-  if (evt.key === 'Enter') {
-      console.log('Enter pressed');
-      evt.preventDefault();
-  }
-    const newRequest = evt.target.value;
-    newRequest ? debounceRepoDate(newRequest) : clearElem(autocomplete);
-    window.addEventListener('keydown', windowEscHandler);
-    window.addEventListener('click', windowClickHandler);
-    search.removeEventListener('click', searchClickHandler);
-}
-
-function formEnterHandler(event){
-    if(event.keyCode =='Enter') 
-      event.preventDefault()
-}
-
-app.addEventListener('keydown', formEnterHandler);
-search.addEventListener('input', searchClickHandler);
+new Search(new ApplicationView());
